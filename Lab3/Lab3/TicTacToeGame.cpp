c450eb9debceaf5a3d29ce3c97ee738911023574
Jerry Kong <jeric05908@gmail.com>
2017-11-09 19:20:04 -0600

    Jerry Kong

--- Lab3/Lab3/TicTacToeGame.cpp
+++ Lab3/Lab3/TicTacToeGame.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 ostream & operator << (ostream &out, const TicTacToeGame &g) {
 	for (int row = g.row; row >= 0; row--) {	// print rows in opposite
-		out << setw(g.longestString) << row;
+		out << row<<setw(1);
 		for (int col = 0; col <= g.col; col++) {
-			out << setw(g.longestString) << g.pieces[col][row];
+			out << g.pieces[col][row]<<setw(g.longestString+1-g.pieces[col][row].size());
 		}
 		out << endl;
 	}
+	out << setw(2);
 	for (int i = 0; i <= g.col; i++) {
-		out << setw(g.longestString) << i;
+		out <<  i<<setw(g.longestString);
 	}
 	out << endl;
 	return out;
